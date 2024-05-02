e44598e11ff7cbfbfb752f2d6d2bc5a996dd8832
hamzashafiq916 <hamzashafiq916@gmail.com>
2024-05-02 15:45:31 +0500

    words counter

---
+++ words counter/index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+console.log("\t\twelcome to words counter");
+console.log("=".repeat(60));
+let answers = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence:  ",
+    }]);
+let words = answers.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(words);
+console.log("=".repeat(60));
+console.log(`words count: ${words.length}`);
